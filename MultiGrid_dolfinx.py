0766ffc264eca246b0c88ccac0428db2a56f9bc4
Nikhil Thakur <nikhil.thakur.mec14@itbhu.ac.in>
2021-02-25 15:49:22 +0000

    Bugs Corrected.

--- MultiGrid_dolfinx.py
+++ MultiGrid_dolfinx.py
@@ -3,13 +3,15 @@ import numpy as np
 import ufl
 import math
 import scipy as scp
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
 from mpi4py import MPI
 from petsc4py import PETSc
 
 
 finest_level = 3
 coarsest_level = 0
-coarsest_level_elements_per_dim = 8
+coarsest_level_elements_per_dim = 4
 coarsest_level_nodes_per_dim = coarsest_level_elements_per_dim + 1
 
 # Defining the Parameters of multigrid
@@ -24,14 +26,14 @@ omega = float(2/3)  # Parameter for Jacobi Smoother
 mesh_dict = {}  # Format : {level : mesh}
 element_size = {}
 mesh_dof_list_dict = {}  # Contains the mesh dictionaries type :: level : mesh_dict
-# Format : {level : (A , level)} Second level is used in V_cycle and FMG to iterate over coarse matrices
+# Format : {level : (A-sparse , level)} level is used in V_cycle and FMG to iterate over coarse matrices
 A_dict = {}
 b_dict = {}  # Format : {level : b}
 A_dict_jacobi = {}  # Format {level : (R_omega , diag_A_inv , level)}
-A_jacobi_store = {}
 a_dolfinx = None
 L_dolfinx = None
 bcs_dolfinx = None
+b_dolfinx = None
 
 
 for i in range(coarsest_level, finest_level + 1):
@@ -42,10 +44,14 @@ for i in range(coarsest_level, finest_level + 1):
     V = dolfinx.FunctionSpace(mesh, ("CG", 1))
     V_dof_coord = V.tabulate_dof_coordinates()
     dof_dict = {}
-    for i in range(0, V.dofmap.index_map.size_local * V.dofmap.index_map_bs):
-        dof_dict[i] = tuple([round(i, 3) for i in V_dof_coord[i]])
-        dof_dict[tuple([round(i, 3) for i in V_dof_coord[i]])] = i
-    mesh_dof_list_dict[i] = (dof_dict, i)
+    for j in range(0, V.dofmap.index_map.size_local * V.dofmap.index_map_bs):
+        # dof_dict[j] = tuple([round(k, 3) for k in V_dof_coord[j]])
+        # dof_dict[tuple([round(k, 3) for k in V_dof_coord[j]])] = j
+        #cord_tup = tuple(cor if cor > 1E-7 else 0 for cor in V_dof_coord[j])
+        cord_tup = tuple(round(cor, 7) for cor in V_dof_coord[j])
+        dof_dict[j] = cord_tup
+        dof_dict[cord_tup] = j
+    mesh_dof_list_dict[i] = dof_dict
 
     uD = dolfinx.Function(V)
     uD.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
@@ -69,6 +75,7 @@ for i in range(coarsest_level, finest_level + 1):
     A_sp = scp.sparse.csr_matrix((av, aj, ai))
     # Stores the Sparse version of the Stiffness Matrices
     A_dict[i] = (A_sp, i)
+    #print('Level {} matrix assembled and its shape is {}'.format(i, A_sp.shape))
 
     if i == finest_level:                           # Storing the finest level vector in b_dict
         L = f * v * ufl.dx
@@ -83,18 +90,22 @@ for i in range(coarsest_level, finest_level + 1):
         b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES,
                       mode=PETSc.ScatterMode.REVERSE)
         dolfinx.fem.set_bc(b, [bc])
-        b_dict[i] = b
+        b_dolfinx = b
+        b_dict[i] = np.array(b.array).reshape(
+            (coarsest_level_elements_per_dim * 2**finest_level + 1) ** 2, 1)
 
 # Defining the matrices for the Jacobi Smoothing
-
+# print(b_dict[finest_level].shape)
 
 # Takes in a Global Stiffness Matrix and gives the Final Jacobi Iteration Matrix
+
+
 def getJacobiMatrices(A):
     A_mat = A[0]
     negative_upper_A = (-1) * scp.sparse.triu(A_mat, k=1)
     negative_lower_A = (-1) * scp.sparse.tril(A_mat, k=-1)
     diag_A_inv = scp.sparse.csr_matrix(
-        np.diag(1 / (A_mat.diagonal().reshape(A_mat.shape[0], 1))))
+        np.diag(1 / (A_mat.diagonal())))
     # only a diagonal matrix so space can be saved here
     # diag_A_inverse = [1 / x for x in diag_A]
     Rj = diag_A_inv.dot((negative_upper_A + negative_lower_A))
@@ -120,7 +131,7 @@ def Interpolation2D(vec_2h, level_coarse):
     # target_dof_size[0] * target_dof_size[1]
     vec_h_dim = (coarsest_level_elements_per_dim *
                  2**(level_coarse + 1) + 1) ** 2
-    vec_h = np.zeros((vec_h_dim))
+    vec_h = np.zeros((vec_h_dim, 1))
 
     # Iterating over the dofs
     for i in range(0, vec_h_dim):
@@ -129,9 +140,10 @@ def Interpolation2D(vec_2h, level_coarse):
         if coarse_dof != -1:                # Cannot directly inject the boundary values not present in coarse grid
             # Directly injecting the coarse dof into the fine dof value
             vec_h[i] = vec_2h[coarse_dof]
+
         else:
-            i_h = fine_coord[0] / element_size_fine
-            j_h = fine_coord[1] / element_size_fine
+            i_h = int(fine_coord[0] / element_size_fine)
+            j_h = int(fine_coord[1] / element_size_fine)
             # interpolation needs to be carried out for the dof which are not the part of the coarse vector
 
             # check if first dimension is odd and second is even
@@ -160,6 +172,7 @@ def Interpolation2D(vec_2h, level_coarse):
             else:
                 i_2h = (i_h - 1) / 2
                 j_2h = (j_h - 1) / 2
+
                 coarse_dof_1 = mesh_dict_coarse[(i_2h * element_size_coarse,
                                                  j_2h * element_size_coarse, 0)]
                 coarse_dof_2 = mesh_dict_coarse[((i_2h + 1) * element_size_coarse,
@@ -184,7 +197,7 @@ def Restriction2D(vec_h, level_fine):
 
     vec_2h_dim = (coarsest_level_elements_per_dim *
                   2**(level_fine - 1) + 1) ** 2
-    vec_2h = np.zeros((vec_2h_dim))
+    vec_2h = np.zeros((vec_2h_dim, 1))
 
     # Iterating over the dofs
     for i in range(0, vec_2h_dim):
@@ -247,13 +260,16 @@ def Restriction2D(vec_h, level_fine):
 
 
 def V_cycle_scheme(A_h, v_h, f_h):
-    v_h = jacobiRelaxation(A_h, v_h, f_h, mu1)
+    # v_h = jacobiRelaxation(A_h, v_h, f_h, mu1)
     # Check if the space is the coarsest
     if(A_h[2] == coarsest_level):
-        v_h = jacobiRelaxation(A_h, v_h, f_h, mu2)
+        v_h = spsolve(A_dict[coarsest_level][0], f_h)
+        # v_h = jacobiRelaxation(A_h, v_h, f_h, mu2)
         return v_h
     else:
-        f_2h = Restriction2D((f_h - np.matmul(A_dict[A_h[2]], v_h)), A_h[2])
+        v_h = jacobiRelaxation(A_h, v_h, f_h, mu1)
+        # f_2h = Restriction2D((f_h - np.matmul(A_dict[A_h[2]], v_h)), A_h[2])
+        f_2h = Restriction2D((f_h - A_dict[A_h[2]][0].dot(v_h)), A_h[2])
         v_2h = np.zeros((f_2h.shape[0], 1))
 
         # Fetch the Smaller discretication matrix
@@ -283,10 +299,10 @@ def FullMultiGrid(A_h, f_h):
         v_h = V_cycle_scheme(A_h, v_h, f_h)
     return v_h
 
+
 # Assemble the matrices in for Jacobi in A_dict_jacobi
 
-
-for key, value in A_dict:
+for key, value in A_dict.items():
     A_dict_jacobi[key] = getJacobiMatrices(value)
 
 # force function assembled and the matrices have been generated. Call the FMG solver with the b vector and store the solution vector
@@ -296,6 +312,10 @@ u = FullMultiGrid(A_dict_jacobi[finest_level], b_dict[finest_level])
 
 
 # Computing the dolfinx solution
-problem = dolfinx.fem.LinearProblem(a, L, bcs=[bcs_dolfinx], petsc_options={
+problem = dolfinx.fem.LinearProblem(a_dolfinx, L_dolfinx, bcs=[bcs_dolfinx], petsc_options={
                                     "ksp_type": "preonly", "pc_type": "lu"})
 uh = problem.solve()
+
+print("Code Working")
+print(uh.compute_point_values())
+print(u)
--- mesh_dict_check.py
+++ mesh_dict_check.py
@@ -1,9 +1,15 @@
 import dolfinx
+import numpy as np
+import ufl
+import math
+import scipy as scp
+from scipy import sparse
 from mpi4py import MPI
 from petsc4py import PETSc
+from numpy.linalg import inv
 
 mesh1 = dolfinx.UnitSquareMesh(
-    MPI.COMM_WORLD, 4, 4, dolfinx.cpp.mesh.CellType.triangle)
+    MPI.COMM_WORLD, 16, 16, dolfinx.cpp.mesh.CellType.triangle)
 mesh2 = dolfinx.UnitSquareMesh(
     MPI.COMM_WORLD, 2, 2, dolfinx.cpp.mesh.CellType.triangle)
 
@@ -25,5 +31,51 @@ for i in range(0, V2.dofmap.index_map.size_local * V2.dofmap.index_map_bs):
     mesh2_dict[i] = tuple([round(i, 3) for i in V2_dof_cord[i]])
     mesh2_dict[tuple([round(i, 3) for i in V2_dof_cord[i]])] = i
 
-print(mesh1_dict[5])
-print(mesh2_dict)
+print(V1_dof_cord[1][1])
+# print(mesh1_dict)
+
+"""
+uD = dolfinx.Function(V)
+uD.vector.set(0.0)
+#uD.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
+uD.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                      mode=PETSc.ScatterMode.FORWARD)
+fdim = mesh.topology.dim-1
+mesh.topology.create_connectivity(fdim, mesh.topology.dim)
+boundary_facets = np.where(np.array(
+    dolfinx.cpp.mesh.compute_boundary_facets(mesh.topology)) == 1)[0]
+boundary_facets_2 = dolfinx.mesh.locate_entities_boundary(
+    mesh, fdim, lambda x: np.isclose(x[0], 1.0))
+boundary_dofs = dolfinx.fem.locate_dofs_topological(
+    V, fdim, boundary_facets_2)
+bc = dolfinx.DirichletBC(uD, boundary_dofs)
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+f = dolfinx.Constant(mesh, -6)
+a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+A = dolfinx.fem.assemble_matrix(a, bcs=[bc])
+A.assemble()
+assert isinstance(A, PETSc.Mat)
+ai, aj, av = A.getValuesCSR()
+A_sp = scp.sparse.csr_matrix((av, aj, ai))
+A_c = A_sp.toarray()
+L = f * v * ufl.dx
+problem = dolfinx.fem.LinearProblem(a, L, bcs=[bc], petsc_options={
+                                    "ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+print(uh.compute_point_values())
+b = dolfinx.fem.create_vector(L)
+with b.localForm() as loc_b:
+    loc_b.set(0)
+    dolfinx.fem.assemble_vector(b, L)
+    # print(b.array)
+    dolfinx.fem.apply_lifting(b, [a], [[bc]])
+    b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES,
+                  mode=PETSc.ScatterMode.REVERSE)
+    dolfinx.fem.set_bc(b, [bc])
+b_np = np.array(b.array).reshape(25, 1)
+u = np.dot(inv(A_c), b_np)
+print(u)
+# print(b_np)
+# print(A_sp.shape)
+"""
